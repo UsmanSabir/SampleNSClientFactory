8f19cf8417c4661efc6c6d6c622fd1c7e92f4d7f
Usman Sabir <hisabir@hotmail.com>
2022-06-03 15:29:00 +0500

    dummy code

---
+++ CommunicationService/Sample/SampleApiGateway/Custom/MyMiddleware.cs
@@ -0,0 +1,195 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Ocelot.Logging;
+using Ocelot.Middleware;
+using System.Diagnostics;
+
+namespace SampleApiGateway.Custom
+{
+    public class MyMiddleware : Ocelot.Middleware.OcelotMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IConfiguration configuration;
+        private readonly IMemoryCache memoryCache;
+        private readonly IOcelotLoggerFactory loggerFactory;
+
+        public MyMiddleware(RequestDelegate next, IConfiguration configuration,
+            IMemoryCache memoryCache, IOcelotLoggerFactory loggerFactory) 
+            : base(loggerFactory.CreateLogger<MyMiddleware>())
+        {
+            _next = next;
+            this.configuration = configuration;
+            this.memoryCache = memoryCache;
+            this.loggerFactory = loggerFactory;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            //await _next.Invoke(httpContext);
+            //    var permissions = new[]
+            //    {
+            //   new ApiPermission()
+            //   {
+            //       PathPattern = "/api/test/values",
+            //       Method = "GET",
+            //       AllowedRoles = ""
+            //   },
+            //   new ApiPermission()
+            //   {
+            //       PathPattern = "/api/test/user",
+            //       Method = "GET",
+            //       AllowedRoles = "User"
+            //   },
+            //   new ApiPermission()
+            //   {
+            //       PathPattern = "/api/test/admin",
+            //       Method = "GET",
+            //       AllowedRoles = "Admin"
+            //   },
+            //};
+
+            var route = httpContext.Items.DownstreamRoute();
+            var host = route.DownstreamAddresses.FirstOrDefault()?.Host;
+            var port = route.DownstreamAddresses.FirstOrDefault()?.Port;
+            var method = route.DownstreamPathTemplate.Value;
+            var body = httpContext.Request.Body;
+            if(route.Key== "sample")
+            {
+                Debug.WriteLine(route.Key);
+            }
+            //var routeKeysConfigs = routeKeys.DownstreamRouteConfig;
+
+            var rout= httpContext.GetRouteData();
+            //var downstreamRouteHolder = httpContext.Items.DownstreamRouteHolder();
+            //if (routeKeysConfigs == null || !routeKeysConfigs.Any())
+            {
+                //var downstreamRouteHolder = httpContext.Items.DownstreamRouteHolder();
+
+                //var tasks = new Task<HttpContext>[downstreamRouteHolder.Route.DownstreamRoute.Count];
+
+                //for (var i = 0; i < downstreamRouteHolder.Route.DownstreamRoute.Count; i++)
+                //{
+                //    var newHttpContext = Copy(httpContext);
+
+                //    newHttpContext.Items
+                //        .Add("RequestId", httpContext.Items["RequestId"]);
+                //    newHttpContext.Items
+                //        .SetIInternalConfiguration(httpContext.Items.IInternalConfiguration());
+                //    newHttpContext.Items
+                //        .UpsertTemplatePlaceholderNameAndValues(httpContext.Items.TemplatePlaceholderNameAndValues());
+                //    newHttpContext.Items
+                //        .UpsertDownstreamRoute(downstreamRouteHolder.Route.DownstreamRoute[i]);
+
+                //    //tasks[i] = Fire(newHttpContext, _next);
+                //}
+
+                //await Task.WhenAll(tasks);
+            }
+                //var rv= httpContext.GetRouteValue(rout.Values);
+                //var downstreamRoute = httpContext.Items.DownstreamRoute();
+                //var a = downstreamRoute.AuthenticationOptions.AuthenticationProviderKey;
+
+                //var result = await httpContext.AuthenticateAsync(downstreamRoute.AuthenticationOptions.AuthenticationProviderKey);
+                //if (result.Principal != null)
+                //{
+                //    httpContext.User = result.Principal;
+                //}
+
+                //var user = httpContext.User;
+                var request = httpContext.Request;
+
+            //httpContext.Items.SetError(new UnauthenticatedError("unauthorized, need login"));
+
+            //var permission = permissions.FirstOrDefault(p =>
+            //    request.Path.ToString().Equals(p.PathPattern, StringComparison.OrdinalIgnoreCase) && p.Method.ToUpper() == request.Method.ToUpper());
+
+            //if (permission == null)
+            //{
+            //    permission =
+            //        permissions.FirstOrDefault(p =>
+            //            Regex.IsMatch(request.Path.ToString(), p.PathPattern, RegexOptions.IgnoreCase) && p.Method.ToUpper() == request.Method.ToUpper());
+            //}
+
+            //if (user.Identity?.IsAuthenticated == true)
+            //{
+            //    if (!string.IsNullOrWhiteSpace(permission?.AllowedRoles) &&
+            //        !permission.AllowedRoles.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+            //            .Any(r => user.IsInRole(r)))
+            //    {
+            //        httpContext.Items.SetError(new UnauthorizedError("forbidden, have no permission"));
+            //        return;
+            //    }
+            //}
+            //else
+            //{
+            //    if (permission != null && string.IsNullOrWhiteSpace(permission.AllowedRoles))
+            //    {
+            //    }
+            //    else
+            //    {
+            //        httpContext.Items.SetError(new UnauthenticatedError("unauthorized, need login"));
+            //        return;
+            //    }
+            //}
+
+            await _next.Invoke(httpContext);
+        }
+
+        private HttpContext Copy(HttpContext source)
+        {
+            var target = new DefaultHttpContext();
+
+            foreach (var header in source.Request.Headers)
+            {
+                target.Request.Headers.TryAdd(header.Key, header.Value);
+            }
+
+            target.Request.Body = source.Request.Body;
+            target.Request.ContentLength = source.Request.ContentLength;
+            target.Request.ContentType = source.Request.ContentType;
+            target.Request.Host = source.Request.Host;
+            target.Request.Method = source.Request.Method;
+            target.Request.Path = source.Request.Path;
+            target.Request.PathBase = source.Request.PathBase;
+            target.Request.Protocol = source.Request.Protocol;
+            target.Request.Query = source.Request.Query;
+            target.Request.QueryString = source.Request.QueryString;
+            target.Request.Scheme = source.Request.Scheme;
+            target.Request.IsHttps = source.Request.IsHttps;
+            target.Request.RouteValues = source.Request.RouteValues;
+            target.Connection.RemoteIpAddress = source.Connection.RemoteIpAddress;
+            target.RequestServices = source.RequestServices;
+            return target;
+        }
+
+        //private async Task Map(HttpContext httpContext, Route route, List<HttpContext> contexts)
+        //{
+        //    if (route.DownstreamRoute.Count > 1)
+        //    {
+        //        var aggregator = _factory.Get(route);
+        //        await aggregator.Aggregate(route, httpContext, contexts);
+        //    }
+        //    else
+        //    {
+        //        MapNotAggregate(httpContext, contexts);
+        //    }
+        //}
+
+        //private void MapNotAggregate(HttpContext httpContext, List<HttpContext> downstreamContexts)
+        //{
+        //    //assume at least one..if this errors then it will be caught by global exception handler
+        //    var finished = downstreamContexts.First();
+
+        //    httpContext.Items.UpsertErrors(finished.Items.Errors());
+
+        //    httpContext.Items.UpsertDownstreamRequest(finished.Items.DownstreamRequest());
+
+        //    httpContext.Items.UpsertDownstreamResponse(finished.Items.DownstreamResponse());
+        //}
+
+        //private async Task<HttpContext> Fire(HttpContext httpContext, RequestDelegate next)
+        //{
+        //    await next.Invoke(httpContext);
+        //    return httpContext;
+        //}
+    }
+}
--- CommunicationService/Sample/SampleApiGateway/Program.cs
+++ CommunicationService/Sample/SampleApiGateway/Program.cs
@@ -1,5 +1,21 @@
+using Ocelot.Claims.Middleware;
 using Ocelot.DependencyInjection;
+using Ocelot.DownstreamPathManipulation.Middleware;
+using Ocelot.DownstreamRouteFinder.Middleware;
+using Ocelot.DownstreamUrlCreator.Middleware;
+using Ocelot.Errors.Middleware;
+using Ocelot.Headers.Middleware;
+using Ocelot.LoadBalancer.Middleware;
 using Ocelot.Middleware;
+using Ocelot.Multiplexer;
+using Ocelot.QueryStrings.Middleware;
+using Ocelot.RateLimit.Middleware;
+using Ocelot.Request.Middleware;
+using Ocelot.Requester.Middleware;
+using Ocelot.RequestId.Middleware;
+using Ocelot.Responder.Middleware;
+using Ocelot.Security.Middleware;
+using SampleApiGateway.Custom;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -35,8 +51,112 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.UseOcelot().Wait();
+var configuration = new OcelotPipelineConfiguration
+{
+    
+    PreErrorResponderMiddleware = async (ctx, next) =>
+    {
+        await next.Invoke();
+    }
+};
+
+app.UseOcelot(
+    (ocelotBuilder, pipelineConfiguration) =>
+{
+    // this sets up the downstream context and gets the config
+    app.UseDownstreamContextMiddleware();
+    // This is registered to catch any global exceptions that are not handled
+    // It also sets the Request Id if anything is set globally
+    ocelotBuilder.UseExceptionHandlerMiddleware();
+
+    // This is registered first so it can catch any errors and issue an appropriate response
+    ocelotBuilder.UseResponderMiddleware();
+    //ocelotBuilder.UseMiddleware<CustomResponseMiddleware>();
+
+    ocelotBuilder.UseDownstreamRouteFinderMiddleware();
+    ocelotBuilder.UseMultiplexingMiddleware();
+
+    // This security module, IP whitelist blacklist, extended security mechanism
+    ocelotBuilder.UseSecurityMiddleware();
+
+    ocelotBuilder.UseHttpHeadersTransformationMiddleware();
+
+    ocelotBuilder.UseDownstreamRequestInitialiser();
+
+    // We check whether the request is ratelimit, and if there is no continue processing
+    ocelotBuilder.UseRateLimiting();
+
+    ocelotBuilder.UseRequestIdMiddleware();
+
+    // ocelotBuilder.Use((ctx, next) =>
+    // {
+    //     ctx.Items.SetError(new UnauthorizedError("No permission"));
+    //     return Task.CompletedTask;
+    // });
+
+    //ocelotBuilder.UseMiddleware<UrlBasedAuthenticationMiddleware>();
+
+    // The next thing we do is look at any claims transforms in case this is important for authorization
+    ocelotBuilder.UseClaimsToClaimsMiddleware();
+
+    // Now we can run the claims to headers transformation middleware
+    ocelotBuilder.UseClaimsToHeadersMiddleware();
+
+    // Allow the user to implement their own query string manipulation logic
+    ocelotBuilder.UseIfNotNull(pipelineConfiguration.PreQueryStringBuilderMiddleware);
+
+    // Now we can run any claims to query string transformation middleware
+    ocelotBuilder.UseClaimsToQueryStringMiddleware();
+
+    ocelotBuilder.UseClaimsToDownstreamPathMiddleware();
+
+    ocelotBuilder.UseLoadBalancingMiddleware();
+    ocelotBuilder.UseDownstreamUrlCreatorMiddleware();
+
+    ocelotBuilder.UseMiddleware<MyMiddleware>();
+
+    ocelotBuilder.UseHttpRequesterMiddleware();
+
+
+    //=-=================================
+
+    // this sets up the downstream context and gets the config
+    //app.UseDownstreamContextMiddleware();
+    //// This is registered to catch any global exceptions that are not handled
+    //// It also sets the Request Id if anything is set globally
+    //ocelotBuilder.UseExceptionHandlerMiddleware();
+
+    //// This is registered first so it can catch any errors and issue an appropriate response
+    ////ocelotBuilder.UseResponderMiddleware();
+    ////ocelotBuilder.UseMiddleware();
+
+    //ocelotBuilder.UseDownstreamRouteFinderMiddleware();
+    //ocelotBuilder.UseMultiplexingMiddleware();
+    //ocelotBuilder.UseDownstreamRequestInitialiser();
+    //ocelotBuilder.UseRequestIdMiddleware();
+
+
+    //ocelotBuilder.UseMiddleware<MyMiddleware>();
+
+    //ocelotBuilder.UseLoadBalancingMiddleware();
+    //ocelotBuilder.UseDownstreamUrlCreatorMiddleware();
+    //ocelotBuilder.UseHttpRequesterMiddleware();
+}
+
+).Wait();
 
 app.MapControllers();
 
 app.Run();
+
+public static class Extensions
+{
+    public static void UseIfNotNull(this IApplicationBuilder builder,
+        Func<HttpContext, Func<Task>, Task> middleware)
+    {
+        if (middleware != null)
+        {
+            builder.Use(middleware);
+        }
+    }
+}
